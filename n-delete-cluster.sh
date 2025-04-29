f48f8053edfbd4d13893d1473d7a410a9953e581
uetsymphonique <minhtran6464.lhp@gmail.com>
2025-04-29 07:44:23 +0700

    add delete pvc

--- n-delete-cluster.sh
+++ n-delete-cluster.sh
@@ -1,1 +1,5 @@
 gcloud container clusters delete $CLUSTER_NAME --zone $ZONE --quiet
+
+gcloud compute disks list --filter="name~^pvc-" --format="value(name,zone)" | while read disk zone; do
+  gcloud compute disks delete "$disk" --zone="$zone" --quiet
+done
--- n-remove-bucket-and-sa.sh
+++ n-remove-bucket-and-sa.sh
@@ -1,2 +1,2 @@
-gcloud iam service-accounts delete pgbackup-sa@$PROJECT_ID.iam.gserviceaccount.com
+gcloud iam service-accounts delete pgbackup-sa@$PROJECT_ID.iam.gserviceaccount.com --quiet
 gsutil rm -r gs://$BUCKET_NAME
